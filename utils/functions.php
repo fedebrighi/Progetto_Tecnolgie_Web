36b8fedcb2e84bac99fbf43c6f4589bf91f92f9e
Federico Brighi <federico.brighi2@studio.unibo.it>
2025-01-07 16:28:38 +0100

    added functions

--- utils/functions.php
+++ utils/functions.php
@@ -4,3 +4,37 @@
             echo " class='active' ";
         }
     }
+    function getIdFromName($name){
+        return preg_replace("/[^a-z]/", '', strtolower($name));
+    }
+
+    function isUserLoggedIn(){
+        return !empty($_SESSION['idautore']);
+    }
+
+    function registerLoggedUser($user){
+        $_SESSION["idautore"] = $user["idautore"];
+        $_SESSION["username"] = $user["username"];
+        $_SESSION["nome"] = $user["nome"];
+    }
+
+    function getEmptyArticle(){
+        return array("idarticolo" => "", "titoloarticolo" => "", "imgarticolo" => "", "testoarticolo" => "", "anteprimaarticolo" => "", "categorie" => array());
+    }
+
+    function getAction($action){
+        $result = "";
+        switch($action){
+            case 1:
+                $result = "Inserisci";
+                break;
+            case 2:
+                $result = "Modifica";
+                break;
+            case 3:
+                $result = "Cancella";
+                break;
+        }
+
+        return $result;
+    }
