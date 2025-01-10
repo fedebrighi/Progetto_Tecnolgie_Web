4ecccf9bb7c58a8c89d175709ba9465738415cbc
Mattia Saponaro <mattia.saponaro2@studio.unibo.it>
2025-01-10 14:21:39 +0100

    Aggiunto teplate base

--- homepage.php
+++ homepage.php
@@ -3,7 +3,8 @@ require_once 'bootstrap.php';
 
 //Base Template
 $templateParams["titolo"] = "PHPint - Home";
+$templateParams["nome"] = "home.php";
 $templateParams["birre"] = $dbh->getAllBeers();
 
-require 'template/home.php';
+require 'template/base.php';
 ?>
